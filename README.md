ce834391fd3b1f3127a28e94dcb52cc88b9dea2a
Ravsimar Singh Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2018-10-24 11:29:58 +0530

    player registration

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Decentralized Quiz App
-------------------------
+
 A quiz app where upto N people can participate, implemented in Solidity
--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -2,11 +2,74 @@ pragma solidity ^0.4.24;
 contract Quiz
 {
     address public moderator;
+    uint public N; // No. of participants
+    uint public pFee; // Participation fee for each player
+    uint public tFee = 0;
+    bool quizEnded = false;
+    mapping(address => uint256) pendingReturns;
 
-    constructor()
+    struct Player
+    {
+        address addr;
+    }
+
+    Player[] players;
+
+    constructor (uint _N, uint _pFee)
     public
     {
         moderator = msg.sender;
+        N = _N;
+        pFee = _pFee;
 
+        emit quizCreated(N, pFee);
+    }
+
+    // Modifiers
+    modifier onlyModerator()
+    {
+        require(msg.sender == moderator, "Only moderator is allowed to call this method");
+        _;
+    }
+    modifier onlyIfTrue(bool x)
+    {
+        require(x == true, "Value of variable should be true");
+        _;
+    }
+    // Events
+    event quizCreated(uint _N, uint _pFee);
+    event playerRegistered(address _addr);
+
+    // Functions
+    function register()
+    payable
+    public
+    {
+        require(players.length < N, "Player space is full");
+        require(msg.value > pFee, "Insufficient funds sent");
+
+        // Units of fee?
+        pendingReturns[msg.sender] = msg.value;
+        // Subtract the  pFee here
+        players.push(Player({
+            addr: msg.sender
+        }));
+
+        emit playerRegistered(msg.sender);
+    }
+
+
+    function withdraw()
+    onlyIfTrue(quizEnded)
+    public
+    returns (bool)
+    {
+        uint256 amount = pendingReturns[msg.sender];
+        if (amount > 0)
+        {
+            pendingReturns[msg.sender] = 0;
+            msg.sender.transfer(amount);
+        }
+        return true;
     }
 }
